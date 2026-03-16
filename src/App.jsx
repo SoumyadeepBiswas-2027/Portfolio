8b90eb09986f1ab209232079b50a691c7e959456
Soumyadeep Biswas <soumyadeep.bs2023@gmail.com>
2026-03-16 23:21:19 +0530

    Styling using React bits implimented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import LightRays from "./BitsStore/LightRays";
+
+
 function App() {
 
   return (
     <>
       <div className="min-h-screen flex flex-col bg-black">
+
+          <LightRays
+          raysOrigin="top-center"
+          raysColor="#ffffff"
+          raysSpeed={1}
+          lightSpread={0.5}
+          rayLength={3}
+          followMouse={true} />        
         <Navbar/>
       </div>
     </>
